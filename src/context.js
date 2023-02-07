098f0fdbdc9bc9ac71d3bf5a5cfbdc0d7ee9bab0
Travis Milne <Travismilne8@gmail.com>
2023-02-07 14:54:37 -0700

    commit

---
+++ src/context.js
@@ -0,0 +1,61 @@
+import React, { useState, useContext, useEffect } from 'react'
+import { useCallback } from 'react'
+import axios from 'axios'
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true)
+  const [searchTerm, setSearchTerm] = useState('')
+  const [cocktails, setCocktails] = useState('')
+
+  const fetchDrinks = useCallback(async () => {
+    setLoading(true)
+    try {
+      const response = await axios(`${url}${searchTerm}`)
+      const { drinks } = response.data
+      if (drinks) {
+        const newDrinks = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            item
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          }
+        })
+        setCocktails(newDrinks)
+      } else {
+        setCocktails([])
+      }
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  })
+  useEffect(() => {
+    fetchDrinks()
+  }, [searchTerm])
+
+  return (
+    <AppContext.Provider
+      value={{
+        loading,
+        cocktails,
+        setSearchTerm,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam ut
+        dolore eum est a assumenda, fugiat necessitatibus praesentium blanditiis
+        nobis atque molestiae quae excepturi tenetur corrupti tempore
+        consequatur in nemo.
+      </p>
+    </section>
+  )
+}
+
+export default About
